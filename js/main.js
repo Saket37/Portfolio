d202d59a60d201f72b0eb65b6b64a03280353619
Saket Anand <66915273+Saket37@users.noreply.github.com>
2025-07-04 22:16:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 // Header Scroll
 let nav = document.querySelector(".navbar");
- window.onscroll = function(){
-    if(document.documentElement.scrollTop>20){
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add("header_scrolled");
-    }else{
+    } else {
         nav.classList.remove("header_scrolled");
-
     }
- }
 
- // Nav Hide
- let navBar = document.querySelectorAll(".nav-link");
- let navCollapse = document.querySelector(".navbar-collapse.collapse");
- navBar.forEach(function(a){
-    a.addEventListener("click",function(){
+    // Hide the collapsed menu on scroll
+    if (navCollapse.classList.contains("show")) {
+        navCollapse.classList.remove("show");
+    }
+}
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
         navCollapse.classList.remove("show")
     })
- })
+})
